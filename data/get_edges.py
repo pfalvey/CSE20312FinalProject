906be276cbd78d7d88efe6ebaff51eb02ebcb7bf
Patrick James Akalaitis Falvey <pfalvey@nd.edu>
2017-04-18 21:58:03 -0400

    start get_edges.py

---
+++ data/get_edges.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python2.7
+
+#generates a text file of adjacent nodes and distances for the graph class
+
+import json
+import requests
+
+cutoff_distance = 1848 #every building within a 1848 foot radius will be an edge
+
+
