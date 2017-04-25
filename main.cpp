859ef9aa76bb5cd0df77627de58158684d903a85
Doug Smith <dougsmith2015@gmail.com>
2017-04-25 15:34:21 -0400

    Added reader for data/locations.txt to main

--- main.cpp
+++ main.cpp
@@ -3,16 +3,19 @@ Main + i/o for map application
 */
 
 #include <iostream>
+#include <fstream>
 #include <stdlib.h>
 
 #include "Graph.h"
 
 void parseFlags();
 void usage(int);
-
+void loadGraph(char *, Graph *);
 
 int main(int argc, char ** argv){
+  char * FNAME = "data/locations.txt";
   Graph g;
+  loadGraph( FNAME, &g );
   if( argc <2 ){
     usage(1);
   }
@@ -30,6 +33,36 @@ void parseFlags(){
 
 }
 
+
+/*
+Open file and read data into graphs
+*/
+void loadGraph( char * fname, Graph * g ){
+  std::ifstream f(fname);
+  std::string ln;
+  
+  while( std::getline( f, ln ) ){
+    std::string key,name;
+    double x, y;
+    int i = 0, old_i = i;
+
+    while( ln.substr(++i,1) != ";" ){}
+    key = ln.substr(old_i,i-old_i);
+    old_i = ++i;
+
+    while( ln.substr(++i,1) != ";" ){}
+    name = ln.substr(old_i,i-old_i);
+    old_i = ++i;
+    
+    while( ln.substr(++i,1) != ";" ){}
+    x = atof( ln.substr(old_i,i-old_i).c_str() );
+    old_i = ++i;
+
+    y = atof( ln.substr(old_i, (ln.size()-old_i ) ).c_str()  );
+    g->insert( key, name, x, y );
+  }
+
+}
 
 /*
 Display usage and exit with the given status
