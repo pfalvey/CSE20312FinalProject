d9a20a8a75609a459b18ae95d54aac7b5ede5ca9
Doug Smith <dougsmith2015@gmail.com>
2017-04-26 21:22:20 -0400

    Restored loadGraph until we decide what our insert will be doing...

--- main.cpp
+++ main.cpp
@@ -4,15 +4,18 @@ Main + i/o for map application
 
 #include <iostream>
 #include <stdlib.h>
+#include <fstream>
 
 #include "Graph.h"
 
 void parseFlags();
 void usage(int);
+void loadGraph( char *, Graph * );
 
 
 int main(int argc, char ** argv){
   Graph g;
+  loadGraph("data/locations.txt", &g);
   g.addedges();
   if( argc <2 ){
     usage(1);
@@ -45,7 +48,38 @@ void usage( int status ){
   exit(status);
 }
 
-
+/* 
+load graph data from locations 
+*/
+void loadGraph( char * fname, Graph * g ){
+   std::ifstream f(fname);
+   std::string ln;
+   
+   while( std::getline( f, ln ) ){
+     std::string key,name;
+     double x, y;
+     int i = 0, old_i = i;
+     //Count to first ';'
+     while( ln.substr(++i,1) != ";" ){}
+     key = ln.substr(old_i,i-old_i);
+     old_i = ++i;
+ 
+     //Count to second ';'
+     while( ln.substr(++i,1) != ";" ){}
+     name = ln.substr(old_i,i-old_i);
+     old_i = ++i;
+     
+     //Count to third ';'
+     while( ln.substr(++i,1) != ";" ){}
+     x = atof( ln.substr(old_i,i-old_i).c_str() );
+     old_i = ++i;
+ 
+     //Count to end
+     y = atof( ln.substr(old_i, (ln.size()-old_i ) ).c_str()  );
+     g->insert( key, name, x, y );
+   }
+ 
+ }
 
 
 
