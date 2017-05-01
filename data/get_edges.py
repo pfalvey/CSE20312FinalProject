b76993885ece4c7d6aa18eda59ccecc0c7b21247
Patrick James Akalaitis Falvey <pfalvey@nd.edu>
2017-05-01 00:05:58 -0400

    added commented-out google api access

--- data/get_edges.py
+++ data/get_edges.py
@@ -7,15 +7,21 @@
 #EDGE DISTANCE_TO_EDGE
 
 import json
-import requests
 import math
 import os
+import urllib
 
 cutoff_distance = 560           #every building within a 560 foot radius will be an edge. 
 #There will be edge cases (no pun intended) for buildings with minimum edges of over 560 feet which will be handled by keeping track of the two shortest edges
 lat_to_feet = 364393.7354401166  #conversion from one degree of latitude to feet at Notre Dame
 long_to_feet = 273092.431579933    #conversion from one degree of longitude to feet at Notre Dame
 
+
+#google maps api keys are stored on the Notre Dame network and can only be accessed on the Notre Dame network. This is for safety/privacy
+placeKeyFile = open('/afs/nd.edu/user36/pfalvey/Public/cse20312project/key1', 'r')
+mapKeyFile = open('/afs/nd.edu/user36/pfalvey/Public/cse20312project/key2', 'r')
+placeKey = placeKeyFile.readline()
+mapKey = mapKeyFile.readline()
 
 
 
@@ -36,19 +42,46 @@ for i in f1:
     shortest_dist_2 = 5281
     shortest_name_1 = ""
     shortest_name_2 = ""
+    #get PlaceID from google api
+    """placeURL = "https://maps.googleapis.com/maps/api/geocode/json?latlng={},{}&key={}".format(i[2],i[3],placeKey)
+    placeURL.strip()
+    r1 = urllib.urlopen(placeURL)
+    jsonn = json.loads(r1.read())
+    print jsonn
+    placeID1= jsonn['results'][0]['place_id']"""
     for j in f2:
         j = j.strip() #formatting the line
         j = j.split(';')
         x1 = float(i[2]) * lat_to_feet #converting coordinates to feet
         x2 = float(j[2]) * lat_to_feet
-        y1 = float(i[3]) * lat_to_feet
-        y2 = float(j[3]) * lat_to_feet
+        y1 = float(i[3]) * long_to_feet
+        y2 = float(j[3]) * long_to_feet
+        #get building place ID from google maps api
+        """    placeURL2 = "https://maps.googleapis.com/maps/api/geocode/json?latlng={},{}&key={}".format(j[2],j[3],placeKey)
+        placeURL2.strip()
+        r2 = urllib.urlopen(placeURL2)
+        jsonn2 = json.loads(r2.read())
+        print i[0], j[0]
+        placeID2 = jsonn2['results'][0]['place_id']"""
+
         #find distance and compare to cutoff distance
         dist = math.hypot(float(x2) - float(x1), float(y2) - float(y1))
         if dist < cutoff_distance and dist > 0:
             name = j[0]
             edges.append(name)
-            distance.append(dist)
+            #get actual distance from google maps api
+            """      mapURL="https://maps.googleapis.com/maps/api/distancematrix/json?origins=place_id:{}&destinations=place_id:{}&mode=walking&units=imperial&key={}".format(placeID1, placeID2, mapKey)
+            mapURL.strip()
+            r3 = urllib.urlopen(mapURL)
+            jsonn3 = json.loads(r3.read())
+            newdist = jsonn3['rows'][0]['elements'][0]['distance']['value']
+            newdist = float(newdist)
+            newdist = newdist * 3
+            if newdist > 0:
+                distance.append(newdist)
+            else:
+                distance.append(dist)"""
+            distance.append(dist) #delete this line when google api works
         #update shortest distance edges
         if dist < shortest_dist_1 and dist > 0:
             shortest_name_2 = shortest_name_1
