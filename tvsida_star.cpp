ef196d1f63acc56ddb74e4bd45239c4072e78266
Ethan Williams <ewilli23@nd.edu>
2017-05-02 00:01:32 -0400

    fixed error checking in tvsida_recursive

--- tvsida_star.cpp
+++ tvsida_star.cpp
@@ -30,7 +30,8 @@ dist Graph::tvsida_star(Node *start, Node *end, dist target) {
 			std::cout << "Error: Path not found" << std::endl;
 			return EXIT_FAILURE;
 		}
-		if (best.first > 0 && std::abs(current.first-target) < std::abs(best.first-target)) best = current;
+		if (best.first <= 0) best = current;
+		else if (std::abs(current.first-target) < std::abs(best.first-target)) best = current;
 		if (best.first - target == 0) break;
 		bound += std::abs(current.first - target);
 	}
@@ -63,8 +64,8 @@ path tvsida_recursive(Graph &g, Node * current, Node * end, dist nodecost, dist 
 		Node * neighbor = g.get(it->first);
 		path possible = tvsida_recursive(g, neighbor, end, nodecost+it->second, bound);
 		possible.second.push_front(current);
-		possible.first += it->second;
 		if (possible.first < 0) continue; //Error on path
+		possible.first += it->second;
 		if (*possible.second.end() == end) {
 			if (best.first == 0) best = possible; //first valid path
 			else if (std::abs(nodecost + possible.first - bound) < std::abs(nodecost + best.first - bound)) best = possible;
